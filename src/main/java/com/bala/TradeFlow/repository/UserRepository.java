1d49fce73e383436a0cc0b6d63b25737d8beaa93
Bala Kumar <balaashok2004@gmail.com>
2025-10-15 20:01:44 +0530

    Added Service layer and custom method in repository

--- src/main/java/com/bala/TradeFlow/repository/UserRepository.java
+++ src/main/java/com/bala/TradeFlow/repository/UserRepository.java
@@ -4,7 +4,12 @@ import com.bala.TradeFlow.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity , Long> {
+
+    boolean isEmailExist(String userEmail);
+    public Optional<UserEntity> findByEmail(String userEmail);
 
 }
