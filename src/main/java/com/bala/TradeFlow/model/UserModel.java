50b8235b2106f23ec1f216b8c2a405dbdffb01b7
Bala Kumar <balaashok2004@gmail.com>
2025-10-15 21:29:12 +0530

    Added getters and setters

--- src/main/java/com/bala/TradeFlow/model/UserModel.java
+++ src/main/java/com/bala/TradeFlow/model/UserModel.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 @Table(name = "users")
 public class UserModel {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column(name="user_name" , nullable = false)
@@ -30,5 +30,45 @@ public class UserModel {
 
     public UserModel(){
 
+    }
+
+    public RoleModel getRole() {
+        return role;
+    }
+
+    public void setRole(RoleModel role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 }
