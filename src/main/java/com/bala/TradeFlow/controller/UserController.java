5f8efb057a586ac488e6a8b34979765d4b008721
Bala Kumar <balaashok2004@gmail.com>
2025-10-15 23:19:05 +0530

    Changed to RequestBody in controller

--- src/main/java/com/bala/TradeFlow/controller/UserController.java
+++ src/main/java/com/bala/TradeFlow/controller/UserController.java
@@ -5,6 +5,7 @@ import com.bala.TradeFlow.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,7 +17,7 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public String registerUser(@RequestParam UserModel userModel){
+    public String registerUser(@RequestBody UserModel userModel){
         userService.registerUser(userModel);
         return "User has been added successfully";
     }
